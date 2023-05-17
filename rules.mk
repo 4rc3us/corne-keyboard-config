44e64d1f6f50698542747361cb5d5b58bcb234a5
Juan Daniel <juandanielarce398@gmail.com>
2023-05-17 17:19:16 -0500

    Unicodemap option

--- rules.mk
+++ rules.mk
@@ -5,6 +5,7 @@ OLED_ENABLE       = yes
 OLED_DRIVER       = SSD1306
 LTO_ENABLE        = yes
 BOOTLOADER        = atmel-dfu
-UNICODE_ENABLE    = yes
+UNICODE_ENABLE    = no
+UNICODEMAP_ENABLE = yes
 
 SRC += ./oled.c ./rgb.c
